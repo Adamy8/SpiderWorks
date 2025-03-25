29e08b78e013dd289f613cf2f6cf0974a32881f5
Adamy <admye@ucdavis.edu>
2025-03-24 21:03:11 -0700

    debuged, pipeline done(cleandata.json)

--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -11,13 +11,13 @@ class BookscraperItem(scrapy.Item):
     name = scrapy.Field()
     pass
 
-def serialize_price(value):
-    return f'£ {str(value)}'
+# def serialize_price(value):
+#     return f'£ {str(value)}'
 
 class BookItem(scrapy.Item):
     url = scrapy.Field()
     title = scrapy.Field()
-    upc = scrapy.Field()  # Universal Product Code
+    upc = scrapy.Field()
     product_type = scrapy.Field()
     price_excl_tax = scrapy.Field()
     price_incl_tax = scrapy.Field()
--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -14,7 +14,7 @@ class BookscraperPipeline:
         adapter = ItemAdapter(item)
 
         # strip all whitespaces from strings
-        field_names - adapter.field_name()
+        field_names = adapter.field_names()
         for field_name in field_names:
             if field_name != 'description':
                 value = adapter.get(field_name)
@@ -25,7 +25,7 @@ class BookscraperPipeline:
         for key in lowercase_keys:
             value = adapter.get(key)
             adapter[key] = value.lower()
-
+ 
         # Price -> float
         price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
         for key in price_keys:
