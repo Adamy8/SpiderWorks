2d384b1856ca1860d85ee0d737c667c82e291ad1
Adamy <admye@ucdavis.edu>
2025-03-24 22:59:46 -0700

    specify output file in spider - custom_settings

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -6,6 +6,12 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
+    custom_settings = {         # this can be set to overwrite the settings.py file (not recommended)
+        'FEEDS': {
+            'bookdata.json': {'format': 'json', 'overwrite': True},
+        },
+    }
+
     def parse(self, response):
         books = response.css("article.product_pod")
         for book in books:
